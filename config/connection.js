88e90c1c6bf1f3cb6cc8308376e02f142329a62e
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-09-18 21:05:20 -0700

    db created & changed some var names

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// var Sequelize = require("sequelize");
+
+
+// // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+// var Connection_Sequelize = new Sequelize("home_users", "root", "Abosaad123", {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
+
+// // Exports the connection for other files to use
+// module.exports = Connection_Sequelize;
