165dc609532d8c0942812d8fff88efbd562a06d5
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-09-27 16:08:50 -0700

    validation form

--- public/js/login.js
+++ public/js/login.js
@@ -1,141 +1,108 @@
 // -----------------------LOGIN---------------------------- //
 
-$(function() {
-  $('#login-form-link').click(function(e) {
+$(function () {
+  $('#login-form-link').click(function (e) {
     $("#login-form").delay(100).fadeIn(100);
-   $("#register-form").fadeOut(100);
-  $('#register-form-link').removeClass('active');
-  $(this).addClass('active');
-  e.preventDefault();
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  $('#register-form-link').click(function (e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
 });
 
-$('#register-form-link').click(function(e) {
-  $("#register-form").delay(100).fadeIn(100);
-   $("#login-form").fadeOut(100);
-  $('#login-form-link').removeClass('active');
-  $(this).addClass('active');
-  e.preventDefault();
-});
-
-});
 
 // -----------------------------LOGIN script-------------------------------//
 
-function validateForm(){
-  event.preventDefault();
-
-  var uname = document.forms["myForm"]["username"].value;
-  var pass = document.forms["myForm"]["password"].value;
-  var API = {
-    saveExample: function (example) {
-      return $.ajax({
-        headers: {
-          "Content-Type": "application/json"
-        },
-        type: "POST",
-        url: "api/examples",
-        data: JSON.stringify(example)
-      });
-    },
-    getExamples: function () {
-      return $.ajax({
-        url: "api/examples",
-        type: "GET"
-      });
-    },
-    deleteExample: function (id) {
-      return $.ajax({
-        url: "api/examples/" + id,
-        type: "DELETE"
-      });
-    }
-  };
-
-
-// User Login/ Sign up
-
+var API = {
+  saveExample: function (example) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "api/examples",
+      data: JSON.stringify(example)
+    });
+  },
+  getExamples: function () {
+    return $.ajax({
+      url: "api/examples",
+      type: "GET"
+    });
+  },
+  deleteExample: function (id) {
+    return $.ajax({
+      url: "api/examples/" + id,
+      type: "DELETE"
+    });
+  }
+};
 
 function logIn(usernameInput, paaswordInput) {
-    API.getExamples().then(function (data) {
-      var currentUser = data.find(function (usersDB) {
-        if (usersDB.userName === usernameInput && usersDB.password === paaswordInput) {
-          return usersDB;
-        }
-      });
-      console.log(currentUser);
-      if (currentUser === undefined) {
-        alert("username or password is incorrect");
-      } else {
-        window.location.replace("http://localhost:3000/homepage");
-        alert("welcome back!");
+  API.getExamples().then(function (data) {
+    var currentUser = data.find(function (usersDB) {
+      if (usersDB.userName === usernameInput && usersDB.password === paaswordInput) {
+        return usersDB;
       }
     });
-  }
-
-  $("#login-submit").on('click', function(){
-      console.log(uname)
-      console.log(pass);
-      logIn(uname, pass);
-
+    console.log(currentUser);
+    if (currentUser === undefined) {
+      alert("username or password is incorrect");
+    } else {
+      window.location.replace("http://localhost:3000/homepage");
+      alert("welcome back!");
+    }
   });
-  
-  function signUp(usernameInput, paaswordInput) {
-    API.getExamples().then(function(data) {
-      var checkUsername = data.find(function(DBusername) {
-        return DBusername.userName === usernameInput;
+}
+
+function signUp(usernameInput, paaswordInput) {
+  API.getExamples().then(function (data) {
+    var checkUsername = data.find(function (DBusername) {
+      return DBusername.userName === usernameInput;
+    });
+    console.log(checkUsername);
+    if (checkUsername === undefined) {
+      alert("username is avaliable");
+      var user = {
+        userName: usernameInput,
+        password: paaswordInput
+      };
+      API.saveExample(user).then(function () {
+        refreshExamples();
       });
-      console.log(checkUsername);
-      if (checkUsername === undefined) {
-        alert("username is avaliable");
-        var user = {
-          userName: usernameInput,
-          password: paaswordInput
-        };
-        API.saveExample(user).then(function() {
-          refreshExamples();
-        });
-        $exampleText.val("");
-        $exampleDescription.val("");
-      } else {
-        alert("username is taken");
-      }
-    });
-  }
-
-
-//Other shit
-
-
-  if((!isEmpty(uname, "Log In")) && (!isEmpty(pass, "Password"))){
-
-console.log("GOOD")
-
-      return true;
-  }else{
-
-      console.log("BAD")
-
-      return false;
-  }
+      $exampleText.val("");
+      $exampleDescription.val("");
+    } else {
+      alert("username is taken");
+    }
+  });
 }
 
-function isEmpty(elemValue, field){
-  if((elemValue == "") || (elemValue == null)){
-      alert("you can not have "+field+" field empty");
-      return true;
-  }else{
-      return false;
-  }
+function register() {
+  $("#register-submit").on("click", function () {
+    var username = $("#username").val();
+    var password = $("#password").val();
+    signUp(username, password);
+  });
 }
 
-// onclick login
-
-//onClick on 'Browse' load the internal page
-$(function(){
-    $('#browse_app').click(function(){
-        $.load('templates/home.html');
-    });
+$("#login-submit").on('click', function () {
+  var uname = $("#login-username").val();
+  var pass = $("#login-password").val();
+  logIn(uname, pass);
 });
 
 
-// ANIMATED TEXT //
+$(".register").on("click", function () {
+  register();
+});
+
